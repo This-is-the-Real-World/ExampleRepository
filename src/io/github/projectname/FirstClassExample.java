8f967922a08d08a1243e25a1d6b07d70d80d4043
Jake Arnold <rakeyjake@hotmail.co.uk>
2014-06-04 17:07:25 +0100

    Added a class, package and documentation examples.

---
+++ src/io/github/projectname/FirstClassExample.java
@@ -0,0 +1,30 @@
+package io.github.projectname;
+
+/**
+ * Class description goes here.
+ *
+ * @author WhoeverCreatedTheClass
+ * @version VERSION NUMBER OF THIS CLASS (OPTIONAL)
+ */
+public class FirstClassExample {
+
+    /**
+     * DESCRIPTION OF WHAT IT DOES
+     *
+     * @param intParam DESCRIPTION OF THE PARAMETER
+     */
+    public void methodNameWithNoReturn(int intParam) {
+
+    }
+
+    /**
+     * DESCRIPTION OF WHAT IT DOES
+     *
+     * @param arg0 DESCRIPTION OF THE PARAMETER
+     * @return A DESCRIPTION OF WHAT IS RETURNED
+     */
+    public int methodNameWithReturn(String arg0) {
+        return -1;
+    }
+
+}
